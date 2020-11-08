be6ef6ed7a14f6d8447f6d2cc71b78a074f3c23e
Durga pavani Brundavanam <durgabr@cisco.com>
2020-11-07 21:41:00 -0800

    Setup React

---
+++ react/jsx/src/index.js
@@ -0,0 +1,10 @@
+//Import React and React DOM dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create react component
+const App = () => {
+    return <div>Hi there! This is Durga.</div>;
+};
+//Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
